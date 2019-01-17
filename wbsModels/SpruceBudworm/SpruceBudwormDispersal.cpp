dda00c3178026e34fc95da76857eb6bc1ac5cac8
Rémi Saint-Amant <Tigroux74@hotmail.com>
2019-01-17 14:59:55 -0500

    Update for adult mortality (no change)

--- wbsModels/SpruceBudworm/SpruceBudwormDispersal.cpp
+++ wbsModels/SpruceBudworm/SpruceBudwormDispersal.cpp
@@ -112,7 +112,8 @@ namespace WBSF
 			//Create stand
 			CSBWStand stand(this);
 			stand.m_bFertilEgg = false;
-			stand.m_bApplyAttrition = true;//change by RSA because seem to be faster with attrition 2018-08-02
+			stand.m_bApplyAttrition = true;
+			stand.m_bApplyAdultAttrition = true;
 			stand.m_defoliation = m_defoliation;
 
 			CSBWTreePtr pTree(new CSBWTree(&stand));
