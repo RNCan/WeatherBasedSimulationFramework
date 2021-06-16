cf0c50c73161f8aab69ae0ab0cfe3f75614d822f
Rémi Saint-Amant <Tigroux74@hotmail.com>
2021-06-16 16:02:52 -0400

    Add GetWindowDPI

--- wbs/src/UI/Common/UtilWin.h
+++ wbs/src/UI/Common/UtilWin.h
@@ -352,6 +352,8 @@ namespace UtilWin
 
 	//BOOL RegisterExtension(CString sExt, CString sDescription);
 	inline DWORD GetWindowStyle(HWND hwnd) { return (DWORD)GetWindowLong(hwnd, GWL_STYLE); }
+	WORD GetWindowDPI(HWND hWnd);
+
 }
 
 
