74f033de82c8710cf0fa77a721cef0e7489c8234
Rémi Saint-Amant <Tigroux74@hotmail.com>
2022-01-19 19:10:12 -0500

    Update equation with the revised article

--- wbsModels/LaricobiusOsakensis/LaricobiusOsakensisEquations.cpp
+++ wbsModels/LaricobiusOsakensis/LaricobiusOsakensisEquations.cpp
@@ -27,27 +27,27 @@ using namespace std;
 namespace WBSF
 {
 
-//*********************************************************************************************
-//fitted parameters for cumulative egg creation from egg and larvae, CEC
-//	NbVal = 30	Bias = -0.16170	MAE = 3.85438	RMSE = 5.36376	CD = 0.98070	R² = 0.98077
-//mu = 1.58011
-//s = 228.2837
-//Th1 = 2.1
-//Th2 = 20.2
+	//*********************************************************************************************
+	//fitted parameters for cumulative egg creation from egg and larvae, CEC
+	//	NbVal = 30	Bias = -0.16170	MAE = 3.85438	RMSE = 5.36376	CD = 0.98070	R² = 0.98077
+	//mu = 1.58011
+	//s = 228.2837
+	//Th1 = 2.1
+	//Th2 = 20.2
 
 
-//*********************************************************************************************
-//fitted parameters for Adult Emergence from Soil AES (from L. nigrinus)
-//#NbVal = 100	Bias = 0.48893	MAE = 5.32455	RMSE = 7.49034	CD = 0.96157	R² = 0.96300
-//lam0 = 121
-//lam1 = 212
-//lam2 = -294.5
-//lam3 = 105.8
-//lam_a = 34.8
-//lam_b = 20
-//mu_ADE = 1157.8
-//s_ADE = 125.0
-//Th_ADE = -2.5
+	//*********************************************************************************************
+	//fitted parameters for Adult Emergence from Soil AES (from L. nigrinus)
+	//#NbVal = 100	Bias = 0.48893	MAE = 5.32455	RMSE = 7.49034	CD = 0.96157	R² = 0.96300
+	//lam0 = 121
+	//lam1 = 212
+	//lam2 = -294.5
+	//lam3 = 105.8
+	//lam_a = 34.8
+	//lam_b = 20
+	//mu_ADE = 1157.8
+	//s_ADE = 125.0
+	//Th_ADE = -2.5
 
 
 
@@ -97,24 +97,27 @@ namespace WBSF
 		};
 
 
-				
-		
-	
-		
+
+
+
+
 		static const array< vector<double>, LOF::NB_STAGES>  P_DEV =
 		{ {
-			//Non-linear
-			{ 4.034685e-02, 8.769218e-02, 3.000000e+00, 2.700000e+01, 9.286385e-01, 5.000079e-01 },//Egg
-			{ 2.027189e-02, 8.476716e-02, 4.000000e+00, 2.700000e+01, 3.597608e-01, 5.000017e-01 },//L1
-			{ 2.027189e-02, 8.476716e-02, 4.000000e+00, 2.700000e+01, 3.597608e-01, 5.000017e-01 },//L2
-			{ 2.027189e-02, 8.476716e-02, 4.000000e+00, 2.700000e+01, 3.597608e-01, 5.000017e-01 },//L3
-			{ 2.027189e-02, 8.476716e-02, 4.000000e+00, 2.700000e+01, 3.597608e-01, 5.000017e-01 },//L4
-			{ 1.050187e-01, 3.292772e-02, 4.000000e+00, 3.300000e+01, 4.994740e+01, 5.421225e+00 },//PrePupae
-			{ 9.747868e-03, 1.092349e-01, 0.000000e+00, 3.300000e+01, 1.000469e-01, 5.229575e-01 },//Pupae
-			{                                                                                    },//aestival diapause adult
-			{ 2.874981e-02, 1.065805e-01, 4.000000e+00, 9.998542e+01                             },//adult
-		} };
+				//Non-linear
+				{ 4.034685e-02, 8.769218e-02, 3.000000e+00, 2.700000e+01, 9.286385e-01, 5.000079e-01 },//Egg
+				{ 2.027189e-02, 8.476716e-02, 4.000000e+00, 2.700000e+01, 3.597608e-01, 5.000017e-01 },//L1
+				{ 2.027189e-02, 8.476716e-02, 4.000000e+00, 2.700000e+01, 3.597608e-01, 5.000017e-01 },//L2
+				{ 2.027189e-02, 8.476716e-02, 4.000000e+00, 2.700000e+01, 3.597608e-01, 5.000017e-01 },//L3
+				{ 2.027189e-02, 8.476716e-02, 4.000000e+00, 2.700000e+01, 3.597608e-01, 5.000017e-01 },//L4
+				{ 9.052423e-02, 4.551370e-02, 4.000000e+00, 3.400000e+01, 4.999814e+01, 7.393725e+00 },//PrePupae
+				{ 9.747868e-03, 1.092349e-01, 0.000000e+00, 3.300000e+01, 1.000469e-01, 5.229575e-01 },//Pupae
+				{                                                                                    },//aestival diapause adult
+				{ 2.327918e-02, 1.065902e-01, 2.000000e+00, 9.996176e+01                             },//adult
+			} };
 
+		 
+			
+			
 		//because the larval stage devrate and mortality was difficulte to estimate, we evaluate larval corrected by percent by stage 
 		static const double PROP_BY_STAGE[LOF::NB_STAGES] = { 1, 0.206, 0.203, 0.227, 0.364, 1, 1, 1, 1 };
 
@@ -141,15 +144,15 @@ namespace WBSF
 		{
 			//Relative devlopement Time (individual varition): sigma
 			//Non-linear
-			{0.13502},//Egg
-			{0.18392},//L1
-			{0.18392},//L2
-			{0.18392},//L3
-			{0.18392},//L4
-			{0.20147},//PrePupae
-			{0.12779},//Pupae
-			{1.00000},//aestival diapause adult
-			{0.40071}//adult
+			{0.1350},//Egg
+			{0.1839},//L1
+			{0.1839},//L2
+			{0.1839},//L3
+			{0.1839},//L4
+			{0.2013},//PrePupae
+			{0.1278},//Pupae
+			{1.0000},//aestival diapause adult
+			{0.4007}//adult
 		};
 
 		boost::math::lognormal_distribution<double> RDR_dist(-WBSF::Square(SIGMA[s]) / 2.0, SIGMA[s]);
@@ -158,7 +161,7 @@ namespace WBSF
 			RDR = exp(SIGMA[s] * boost::math::quantile(RDR_dist, m_randomGenerator.Randu()));
 
 		_ASSERTE(!_isnan(RDR) && _finite(RDR));
-		
+
 		return RDR;
 	}
 
@@ -168,7 +171,7 @@ namespace WBSF
 		boost::math::weibull_distribution<double> egg_creation_dist(m_CEC[μ], m_CEC[ѕ]);
 		//boost::math::non_central_f_distribution<double> egg_creation_dist(m_CEC[μ], m_CEC[ѕ], m_CEC[ʎf]);
 		double CDD = boost::math::quantile(egg_creation_dist, m_randomGenerator.Rand(0.001, 0.999));
-		
+
 		return CDD;
 	}
 
@@ -198,33 +201,34 @@ namespace WBSF
 	{
 		static const array<CSurvivalEquation::TSurvivalEquation, LOF::NB_STAGES> S_EQ =
 		{
-			CSurvivalEquation::Survival_12, //egg
-			CSurvivalEquation::Survival_09, //L1
-			CSurvivalEquation::Survival_09, //L2
-			CSurvivalEquation::Survival_09, //L3
-			CSurvivalEquation::Survival_09, //L4
-			CSurvivalEquation::Survival_14,	//PrePupa
-			CSurvivalEquation::Survival_13,	//Pupa
+			CSurvivalEquation::Survival_07, //egg
+			CSurvivalEquation::Survival_11, //L1
+			CSurvivalEquation::Survival_11, //L2
+			CSurvivalEquation::Survival_11, //L3
+			CSurvivalEquation::Survival_11, //L4
+			CSurvivalEquation::Survival_04,	//PrePupa
+			CSurvivalEquation::Survival_14,	//Pupa
 			CSurvivalEquation::Unknown,		//aestival diapause adult
 			CSurvivalEquation::Unknown,		// adult
 		};
 
 
+
 		static const array< vector<double>, LOF::NB_STAGES>  P_SUR =
 		{ {
-			{+5.245993e+00, +3.051113e+00, +3.303175e+01, +3.197979e-01, 4.203672e+02},//egg
-			{+9.779898e-01, +9.284325e-01, -1.356386e+01, -1.034326e-03, 3.889027e-01, 1.110094e+00},//L1
-			{+9.779898e-01, +9.284325e-01, -1.356386e+01, -1.034326e-03, 3.889027e-01, 1.110094e+00},//L2
-			{+9.779898e-01, +9.284325e-01, -1.356386e+01, -1.034326e-03, 3.889027e-01, 1.110094e+00},//L3
-			{+9.779898e-01, +9.284325e-01, -1.356386e+01, -1.034326e-03, 3.889027e-01, 1.110094e+00},//L4
-			{-9.350661e+01, -7.338661e+01, +7.080686e+01, +1.199692e+00, 1.326448e+03},//PrePupa
-			{+9.952444e-01, +3.471469e+00, +2.855358e+01, +1.896005e+00},//Pupa
+			{ 7.101617e-01, -1.724674e-03, 2.814947e-01, 2.572669e-02},//egg
+			{ 1.107977e-02, +1.838004e+01, 1.000000e+03, 1.736085e+00 },//L1
+			{ 1.107977e-02, +1.838004e+01, 1.000000e+03, 1.736085e+00 },//L2
+			{ 1.107977e-02, +1.838004e+01, 1.000000e+03, 1.736085e+00 },//L3
+			{ 1.107977e-02, +1.838004e+01, 1.000000e+03, 1.736085e+00 },//L4
+			{ 9.907457e-01, +2.543370e-01, 3.333060e-06, 6.471209e+01 },//PrePupa
+			{ 9.952141e-01, +3.495538e+00, 2.853136e+01, 1.889230e+00 },//Pupa
 			{},
 			{}
 		} };
 
 		double sr = max(0.0, min(1.0, CSurvivalEquation::GetSurvival(S_EQ[s], P_SUR[s], T)));
-		
+
 		_ASSERTE(!_isnan(sr) && _finite(sr) && sr >= 0 && sr <= 1);
 		return sr;
 	}
@@ -253,9 +257,9 @@ namespace WBSF
 		//while (Fi < 2 || Fi>120)
 		//	Fi = m_randomGenerator.RandUnbiasedLogNormal(log(Fo), sigma);
 
-		boost::math::lognormal_distribution<double> fecondity(log(Fo)-WBSF::Square(sigma) / 2.0, sigma);
+		boost::math::lognormal_distribution<double> fecondity(log(Fo) - WBSF::Square(sigma) / 2.0, sigma);
 		double Fi = boost::math::quantile(fecondity, m_randomGenerator.Rand(0.001, 0.999));
-		
+
 		ASSERT(!_isnan(Fi) && _finite(Fi));
 
 
@@ -267,8 +271,8 @@ namespace WBSF
 		//AICc,maxLL
 		//1698.68,-839.968
 		static const CDevRateEquation::TDevRateEquation P_EQ = CDevRateEquation::Taylor_1981;
-		
-		static const vector<double> P_FEC = { 1.517823e-02, 1.090325e+01, 6.535087e+00 };
+
+		static const vector<double> P_FEC = { 0.01518, 10.9, 6.535 };
 		double r = max(0.0, CDevRateEquation::GetRate(P_EQ, P_FEC, T));
 
 		_ASSERTE(!_isnan(r) && _finite(r) && r >= 0);
